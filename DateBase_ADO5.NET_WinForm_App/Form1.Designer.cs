0dbe36d965d282d996a6cc3fbc95676a479dff98
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-11-13 00:49:02 +0300

    Добавьте файлы проекта.

---
+++ DateBase_ADO5.NET_WinForm_App/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace DateBase_ADO5.NET_WinForm_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textAveScore = new System.Windows.Forms.TextBox();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.DataGridViewTable = new System.Windows.Forms.DataGridView();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Идентификатор";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(27, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(27, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(27, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Средний балл";
+            // 
+            // textId
+            // 
+            this.textId.BackColor = System.Drawing.SystemColors.Window;
+            this.textId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textId.Location = new System.Drawing.Point(237, 30);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(154, 27);
+            this.textId.TabIndex = 4;
+            // 
+            // textLastName
+            // 
+            this.textLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textLastName.Location = new System.Drawing.Point(237, 82);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(154, 27);
+            this.textLastName.TabIndex = 7;
+            // 
+            // textAveScore
+            // 
+            this.textAveScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textAveScore.Location = new System.Drawing.Point(237, 180);
+            this.textAveScore.Name = "textAveScore";
+            this.textAveScore.Size = new System.Drawing.Size(154, 27);
+            this.textAveScore.TabIndex = 8;
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textFirstName.Location = new System.Drawing.Point(237, 130);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(154, 27);
+            this.textFirstName.TabIndex = 9;
+            // 
+            // DataGridViewTable
+            // 
+            this.DataGridViewTable.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.DataGridViewTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DataGridViewTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridViewTable.Location = new System.Drawing.Point(450, 28);
+            this.DataGridViewTable.Name = "DataGridViewTable";
+            this.DataGridViewTable.RowHeadersWidth = 51;
+            this.DataGridViewTable.RowTemplate.Height = 24;
+            this.DataGridViewTable.Size = new System.Drawing.Size(498, 502);
+            this.DataGridViewTable.TabIndex = 10;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonInsert.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.buttonInsert.Location = new System.Drawing.Point(31, 237);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(120, 37);
+            this.buttonInsert.TabIndex = 11;
+            this.buttonInsert.Text = "Добавить";
+            this.buttonInsert.UseVisualStyleBackColor = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.buttonDelete.Location = new System.Drawing.Point(308, 237);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(120, 37);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUpdate.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.buttonUpdate.Location = new System.Drawing.Point(169, 237);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(120, 37);
+            this.buttonUpdate.TabIndex = 13;
+            this.buttonUpdate.Text = "Изменить";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 577);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.DataGridViewTable);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.textAveScore);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridViewTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textAveScore;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.DataGridView DataGridViewTable;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+    }
+}
+
---
+++ DateBase_ADO5.NET_WinForm_App/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DateBase_ADO5.NET_WinForm_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+
+        private void GetList()
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\andre\source\repos\DateBase_ADO5.NET_WinForm_App\DateBase_ADO5.NET_WinForm_App\dbSchool.mdf;Integrated Security=True");
+            da = new SqlDataAdapter("SELECT * FROM Student", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Student");
+            DataGridViewTable.DataSource = ds.Tables["Student"];
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetList();
+        }
+    }
+}
